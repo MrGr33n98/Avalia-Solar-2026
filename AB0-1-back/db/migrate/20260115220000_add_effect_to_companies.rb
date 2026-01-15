19f3c5825cbb838b5aaf8f3e645c47dcaa2c82de
Bobi <felipehhenriquee@gmail.com>
2026-01-15 18:55:34 -0300

    fix migration and

---
+++ AB0-1-back/db/migrate/20260115220000_add_effect_to_companies.rb
@@ -0,0 +1,6 @@
+class AddEffectToCompanies < ActiveRecord::Migration[7.0]
+  def change
+    add_column :companies, :effect, :boolean, null: false, default: false
+    add_index :companies, :effect
+  end
+end
---
+++ AB0-1-back/delete_old_migration.bat
@@ -0,0 +1,3 @@
+@echo off
+del "C:\Users\Bobi\Desktop\AB0-1-main\AB0-1-back\db\migrate\202512280001_add_effect_to_companies.rb"
+echo Old migration file deleted successfully!
