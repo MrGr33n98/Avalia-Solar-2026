48a5ecc469afb27a241c55126bc0721061de6eda
Bobi <felipehhenriquee@gmail.com>
2026-01-15 20:05:18 -0300

    fix files

--- AB0-1-back/db/migrate/20260115220000_add_effect_to_companies.rb
+++ AB0-1-back/db/migrate/20260115220000_add_effect_to_companies.rb
@@ -1,6 +1,8 @@
-class AddEffectToCompanies < ActiveRecord::Migration[7.0]
+class AddEffectFlagToCompanies < ActiveRecord::Migration[7.0]
   def change
-    add_column :companies, :effect, :boolean, null: false, default: false
-    add_index :companies, :effect
+    unless column_exists?(:companies, :effect)
+      add_column :companies, :effect, :boolean, null: false, default: false
+      add_index :companies, :effect
+    end
   end
 end
