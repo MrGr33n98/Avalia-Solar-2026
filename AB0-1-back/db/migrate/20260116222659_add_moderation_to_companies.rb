8d30b6cc60572614165ba8bb84887743bd571b28
Bobi <felipehhenriquee@gmail.com>
2026-01-17 12:38:09 -0300

    fix migration file

--- AB0-1-back/db/migrate/20260116222659_add_moderation_to_companies.rb
+++ AB0-1-back/db/migrate/20260116222659_add_moderation_to_companies.rb
@@ -1,9 +1,17 @@
 class AddModerationToCompanies < ActiveRecord::Migration[7.0]
-  def change
-    add_column :companies, :moderation_status, :string
-    add_column :companies, :submitted_at, :datetime
-    add_column :companies, :approved_at, :datetime
-    add_column :companies, :approved_by_admin_user_id, :integer
-    add_column :companies, :rejected_reason, :text
+  def up
+    add_column :companies, :moderation_status, :string unless column_exists?(:companies, :moderation_status)
+    add_column :companies, :submitted_at, :datetime unless column_exists?(:companies, :submitted_at)
+    add_column :companies, :approved_at, :datetime unless column_exists?(:companies, :approved_at)
+    add_column :companies, :approved_by_admin_user_id, :integer unless column_exists?(:companies, :approved_by_admin_user_id)
+    add_column :companies, :rejected_reason, :text unless column_exists?(:companies, :rejected_reason)
+  end
+
+  def down
+    remove_column :companies, :moderation_status if column_exists?(:companies, :moderation_status)
+    remove_column :companies, :submitted_at if column_exists?(:companies, :submitted_at)
+    remove_column :companies, :approved_at if column_exists?(:companies, :approved_at)
+    remove_column :companies, :approved_by_admin_user_id if column_exists?(:companies, :approved_by_admin_user_id)
+    remove_column :companies, :rejected_reason if column_exists?(:companies, :rejected_reason)
   end
 end
