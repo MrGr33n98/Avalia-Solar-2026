65900e1cf7584497be7e19dfd5777dce40206440
Bobi <felipehhenriquee@gmail.com>
2026-03-06 01:13:18 -0300

    fix: change migration to use change method instead of up/down for concurrent index

--- AB0-1-back/db/migrate/20260306034200_add_companies_ranking_index.rb
+++ AB0-1-back/db/migrate/20260306034200_add_companies_ranking_index.rb
@@ -1,15 +1,11 @@
 class AddCompaniesRankingIndex < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
 
-  def up
+  def change
     add_index :companies, 
       [:status, :rating_avg, :reviews_count], 
       name: 'idx_companies_ranking',
       algorithm: :concurrently,
       if_not_exists: true
-  end
-
-  def down
-    remove_index :companies, name: 'idx_companies_ranking', if_exists: true
   end
 end
