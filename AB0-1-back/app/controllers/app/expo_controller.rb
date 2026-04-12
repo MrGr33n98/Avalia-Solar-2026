bf715e13880aa33120d04d74f0320bdbacce62ef
Bobi <felipehhenriquee@gmail.com>
2026-04-12 12:31:08 -0300

    feat(expo): implement new landing page inspired by OpenSolar

--- AB0-1-back/app/controllers/app/expo_controller.rb
+++ AB0-1-back/app/controllers/app/expo_controller.rb
@@ -1,5 +1,7 @@
 module App
   class ExpoController < BaseController
+    layout 'expo'
+
     # Landing pública — não requer autenticação
     skip_before_action :authenticate_user!
     skip_before_action :verify_authenticity_token, only: [:index]
