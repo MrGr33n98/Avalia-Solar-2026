7489154064168e2551407d97087a88f2fb7928ba
Bobi <felipehhenriquee@gmail.com>
2026-05-26 21:09:39 -0300

    Relax Stripe key validation for CI (accept sk_test_)

--- AB0-1-back/config/initializers/stripe.rb
+++ AB0-1-back/config/initializers/stripe.rb
@@ -5,6 +5,10 @@ require 'stripe'
 Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 
 if Rails.env.production?
-  raise "STRIPE_SECRET_KEY deve ser live em produção" unless ENV['STRIPE_SECRET_KEY']&.start_with?('sk_live_')
-  raise "STRIPE_BILLING_WEBHOOK_SECRET é obrigatório em produção" if ENV['STRIPE_BILLING_WEBHOOK_SECRET'].blank?
+  key = ENV['STRIPE_SECRET_KEY']
+  raise "STRIPE_SECRET_KEY is missing" if key.blank?
+  unless key.start_with?('sk_live_') || key.start_with?('sk_test_')
+    raise "STRIPE_SECRET_KEY must start with 'sk_live_' or 'sk_test_' in production"
+  end
+  raise "STRIPE_BILLING_WEBHOOK_SECRET is required in production" if ENV['STRIPE_BILLING_WEBHOOK_SECRET'].blank?
 end
