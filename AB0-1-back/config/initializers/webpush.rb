a0a35b0ce1293695f7a03052825f8392491d6f96
MrGr22n98 <felipehhenriquee@gmail.com>
2026-08-09 10:35:56 -0300

    fix: remove invalid Webpush.config to prevent rails boot crash

--- AB0-1-back/config/initializers/webpush.rb
+++ AB0-1-back/config/initializers/webpush.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-Webpush.config do |config|
-  config.public_key = ENV.fetch('VAPID_PUBLIC_KEY', '')
-  config.private_key = ENV.fetch('VAPID_PRIVATE_KEY', '')
-  config.subject = ENV.fetch('VAPID_SUBJECT', 'mailto:suporte@avaliasolar.com.br')
-end
+# The ruby webpush gem does not have a Webpush.config method.
+# Store these in Rails configuration instead for later use when calling Webpush.payload_send:
+Rails.application.config.x.webpush.public_key = ENV.fetch('VAPID_PUBLIC_KEY', '')
+Rails.application.config.x.webpush.private_key = ENV.fetch('VAPID_PRIVATE_KEY', '')
+Rails.application.config.x.webpush.subject = ENV.fetch('VAPID_SUBJECT', 'mailto:suporte@avaliasolar.com.br')
